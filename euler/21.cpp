9e9f879ace1f93ce094f5308f508bbee3b258d52
Pavel Kostikov <pkostikov@yandex-team.ru>
2015-12-05 17:55:09 +0300

    21st problem

---
+++ euler/21.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include <map>
+
+int d(int n)
+{
+    auto limit = n / 2;
+    int sum = 0;
+    for (int cand = 1; cand <= limit; ++cand) {
+        if (n % cand == 0) {
+            sum += cand;
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    std::map<int, int> n_to_d;
+    int maxN = 10000;
+    for (int n = 1; n <= maxN; ++n) {
+        n_to_d[n] = d(n);
+    }
+    int sum = 0;
+    for (int n = 1; n <= maxN; ++n) {
+        auto dn = n_to_d[n];
+        if (dn == n) {
+            continue;
+        }
+        if (dn > maxN) {
+            continue;
+        }
+        if (n_to_d[dn] == n) {
+            std::cout << n << ": " << dn << std::endl;
+            sum += n;
+        }
+    }
+    std::cout << "sum = " << sum << std::endl;
+    return 0;
+}
