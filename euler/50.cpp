918d20b5e524561ee38c68141f9463e3e8539880
Pavel Kostikov <pkostikov@yandex-team.ru>
2015-12-15 16:27:29 +0300

    improve 50th problem

--- euler/50.cpp
+++ euler/50.cpp
@@ -15,32 +15,20 @@ int main()
     Natural bestLen = 0;
     Natural bestPrime = 0;
     for (size_t left = 0; left < primes.size(); ++left) {
-        if (primes[left] * bestLen > max) {
-            continue;
-        }
 
-        for (size_t right = left + 1; right < primes.size(); ++right) {
-            auto len = right - left;
-            if (len <= bestLen) {
-                continue;
+        Natural sum = 0;
+        for (size_t i = left; i < primes.size(); ++i) {
+            sum += primes[i];
+            if (sum > max) {
+                break;
             }
 
-            Natural sum = 0;
-            bool breaked = false;
-            for (Natural i = left; i < right; ++i) {
-                sum += primes[i];
-                if (sum > max) {
-                    breaked = true;
-                    break;
+            auto len = i - left + 1;
+            if (len > bestLen) {
+                if (std::binary_search(primes.begin(), primes.end(), sum)) {
+                    bestLen = len;
+                    bestPrime = sum;
                 }
-            }
-            if (breaked) {
-                continue;
-            }
-
-            if (std::binary_search(primes.begin(), primes.end(), sum)) {
-                bestLen = len;
-                bestPrime = sum;
             }
         }
     }
